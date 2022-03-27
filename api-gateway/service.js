e6a6d4e9d8ca03d5e2d060d9c164c2ae70edf638
Juubes <grumpt1@gmail.com>
2022-03-27 23:54:21 +0300

    Removed template resources

---
+++ api-gateway/service.js
@@ -0,0 +1,44 @@
+const https = require("https");
+const express = require("express");
+const proxy = require("http-proxy");
+
+const app = express();
+
+const { ORDER_API_URL, AUTH_API_URL, RABBITMQ_URL } = process.env;
+
+const orderService = proxy.createProxyServer({
+  secure: true,
+  changeOrigin: true,
+  target: ORDER_API_URL,
+});
+
+const authService = proxy.createProxyServer({
+  secure: true,
+  changeOrigin: true,
+  target: AUTH_API_URL,
+});
+
+const rabbitmqService = proxy.createProxyServer({
+  secure: true,
+  changeOrigin: true,
+  target: RABBITMQ_URL,
+});
+
+app.all("/order", (req, res) => {
+  // TODO: Auth, ratelimiting
+  orderService.web(req, res);
+});
+
+app.all("/sandwich", (req, res) => {
+  // TODO: Auth, ratelimiting
+  sandwichService.web(req, res);
+});
+
+app.all("/user", (req, res) => {
+  // TODO: Auth, ratelimiting
+  orderService.web(req, res);
+});
+
+https.createServer(app).listen(443, () => {
+  console.log("Listening on port 443");
+});
