2eaa391d4529457accf9c2e619afcf0d55396cee
Juubes <grumpt1@gmail.com>
2022-04-12 13:31:37 +0300

    WIP Prepared RabbitMQ connections

--- sandwich-service/service.js
+++ sandwich-service/service.js
@@ -1,2 +1,26 @@
 // TODO: No REST, only RabbitMQ
-const rabbitMQURL = "http://127.0.0.1:5672/";
+const amqplib = require("amqplib");
+
+const rabbitMQURL = "amqp://localhost:5672/";
+
+const TODO_QUEUE = "sandwiches-todo";
+const DONE_QUEUE = "sandwiches-done";
+
+(async () => {
+  const connection = await amqplib.connect(rabbitMQURL);
+  const channel = await connection.createChannel();
+
+  // channel.assertExchange()
+  await channel.assertQueue(TODO_QUEUE);
+
+  setInterval(async () => {
+    const data = Buffer.from(`sandwich-${Math.random()}`, "utf-8")
+    channel.sendToQueue(TODO_QUEUE, data, { persistent: true })
+  }, 1000);
+
+  channel.consume(TODO_QUEUE, (msg) => {
+    console.log("Arrived message: ");
+    console.log(msg.content.toString());
+    channel.ack(msg);
+  });
+})()
